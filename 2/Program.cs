93266b29d9d43569daaa89cdb92861c081aee1bf
Vitaliy Novozhilov <don@vitalino.ru>
2022-08-17 00:02:19 +0300

    Home work 2

---
+++ 2/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Clear();
+int num = GetNumberFromUser("Введите целое число A: ","Ошибка ввода!");
+Console.WriteLine("сумма цифр в числе А");
+Console.WriteLine($"\t{num} -> {GetSumDigits(num)}");
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+// Возвращает сумму цифр в числе number добавил обработку отрицательных чисел
+int GetSumDigits(int number)
+{
+    if (number>0)
+    {
+        int sum = 0;
+        while(number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+    else
+    {
+        int sum = 0;
+        while(number < 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+}
