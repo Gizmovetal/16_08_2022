aee8caeb328fc8608d3ab981810da1108676287a
Vitaliy Novozhilov <don@vitalino.ru>
2022-08-16 23:48:32 +0300

    Home work 1

---
+++ 1/Program.cs
@@ -0,0 +1,26 @@
+﻿int GetNumberFromUser(string message, string errorMessage)
+{ 
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int toThePower(int a, int b)
+{   
+    int tempA = a;
+    for (int i = 1; i < b; i++)
+    {
+        a = a*tempA; 
+    }
+    return a;
+}
+
+int A = GetNumberFromUser("Введите целое число A: ","Ошибка ввода!");
+int B = GetNumberFromUser("Введите целое число B: ","Ошибка ввода!");
+
+Console.WriteLine($"{A} в степени {B} = {toThePower(A, B)}");
