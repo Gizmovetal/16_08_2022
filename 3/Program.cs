bfe5b25c27012a157d56c2a1e8061ce50e72788f
Vitaliy Novozhilov <don@vitalino.ru>
2022-08-17 01:01:48 +0300

    Home work 3

---
+++ 3/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+int[] array = new int[8];
+int num = 0;
+int count = array.Length;
+for (int i = 0; i < count; i++)
+    {
+        array[i]= GetNumberFromUser($"Введите {i}-е число в массиве ","Ошибка ввода!");
+    }   
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+
+void PtintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} , ");
+    }
+        Console.WriteLine();
+}
+
+PtintArray(array);   
